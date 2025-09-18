74bf843e1ce19c1dbefd38a4f03d655822ecae74
huynhanhkhoa <huynhanhkhoait@gmail.com>
2025-09-18 15:32:17 +0700

    4

--- assets/js/end.js
+++ assets/js/end.js
@@ -21,19 +21,19 @@ username.addEventListener("keyup", () => {
 saveHighScore = e => {
   const submissionTime = new Date().toISOString();
   e.preventDefault();
+  
   fetch('https://default810604757e7f4ede8d8dbf61f53ca5.28.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/d7d70bbb3ab14a008c7778e43d8a145a/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=LM2xlC3RCxGZSQY3EucnesZZdb6kuG1VL18cC-KRB9k', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      
       'Họ và tên': document.getElementById('username').value,
         'Email cá nhân': document.getElementById('email').value,
         'Thời gian nộp phiếu': submissionTime,
         'Line': document.getElementById("line").value,
-        'Thời gian sử dụng': document.getElementById("thoi_gian_su_dung").value,
-        'Số câu đúng': document.getElementById("so_cau_dung").value,
+        'Thời gian sử dụng': thoi_gian_su_dung_luu,
+        'Số câu đúng': so_cau_dung_luu,
     })
 }).then(res => res.json())
     .then(data => console.log(data))
